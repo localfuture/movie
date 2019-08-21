b22e6eef897ad6d751fd8a4652b0958d72f58393
localfuture <anandalex1993@gmail.com>
2019-08-21 08:14:33 +0530

    read write search delete complete

---
+++ app.js
@@ -0,0 +1,130 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const request = require('request');
+
+const app = express();
+
+
+app.set('view engine','ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+mongoose.connect("mongodb://localhost:27017/movieDB",{useNewUrlParser: true});
+
+const movieschema = mongoose.Schema({
+    movie: String,
+    producer: String,
+    director: String,
+    actor: String,
+    actress: String,
+    year: String,
+    language: String,
+    editor: String,
+    camera: String,
+    dist: String
+
+});
+
+const movieCollection = mongoose.model('movieDetails',movieschema);
+
+app.get("/",(req,res)=>{
+    res.render('movies',{title:"Movies"});
+});
+
+app.get("/add",(req,res)=>{
+    res.render('addmovies',{title:"Add Movies"});
+});
+
+app.post("/add",(req,res)=>{
+ var movie = new movieCollection(req.body);
+ movie.save((error)=>{
+     if(error){
+         console.log('error occured');
+     }else {
+        console.log('Movie Added');
+     }
+ });
+});
+
+app.get("/getdata",(req,res)=>{
+    movieCollection.find((error,data)=>{
+        if(error){
+            console.log(error);
+        }else{
+            res.send(data);
+        }
+    });
+});
+
+
+const display = "http://localhost:3000/getdata";
+
+app.get("/view",(req,res)=>{
+    request(display,(error,Response,body)=>{
+        console.log("Error :", error);
+        console.log("Response :",Response);
+        var data = JSON.parse(body);
+        res.render("viewmovies",{title:"Movie List", data:data});
+        
+    });
+});
+
+app.get("/search",(req,res)=>{
+    res.render("searchmovies",{title:"Search Movies"});
+});
+
+app.get("/sea/:y",(req,res)=>{
+    var f = req.params.y;
+    movieCollection.find({movie:f},(error,data)=>{
+        console.log("API passed DATA");
+        res.send(data);
+    });
+});
+
+
+const sea = "http://localhost:3000/sea/";
+
+app.post("/find",(req,res)=>{
+    var x = req.body.name;
+    console.log(x);
+    request(sea+x,(error,response,body)=>{
+        console.log(error);
+        console.log(response);
+        var d = JSON.parse(body);
+        res.render("viewmovies",{title:"Search Result", data:d});
+    });
+
+});
+
+app.get("/delete",(req,res)=>{
+    res.render("deletemovies",{title:"Delete Movies"});   
+});
+
+app.get("/delet/:id",(req,res)=>{
+    var x = req.params.id;
+    movieCollection.deleteOne({movie:x},(error)=>{
+        if(error){
+            console.log(error);
+        }else{
+            console.log("Sucessfully Deleted");
+        }
+    });
+})
+
+
+const dele = "http://localhost:3000/delet/";
+
+
+app.post("/del",(req,res)=>{
+    var del = req.body.name;
+    request(dele+del,(error,response,body)=>{
+
+    })
+    
+})
+
+
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log("SERVER IS UP AND LISTENING");
+});
